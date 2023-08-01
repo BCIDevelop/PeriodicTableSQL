95422a777505de29728fe9b33503cefe3a62d47a
BCIDevelop <luis.lopez@utec.edu.pe>
2023-08-01 22:45:52 +0000

    fix: Argument changed for $1

--- element.sh
+++ element.sh
@@ -2,7 +2,5 @@
 echo -e "\nPlease provide an element as an argument.\n"
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
 
-read USER_INPUT
-
-ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements E INNER JOIN properties USING(atomic_nunber) INNER JOIN types USING(type_id) WHERE E.atomic_number=$USER_INPUT OR symbol='$USER_INPUT' OR name='$USER_INPUT'")
+ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements E INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE E.atomic_number=$1 OR symbol='$1' OR name='$1'")
 echo $ELEMENT
