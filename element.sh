6d3dbf3fcaaa7f7ef1519ea3b943b2bc509f4519
BCIDevelop <luis.lopez@utec.edu.pe>
2023-08-01 23:40:41 +0000

    fix: SQL fixed

--- element.sh
+++ element.sh
@@ -3,11 +3,18 @@ if [[ $1 ]]
 then
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
-ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius,symbol FROM elements E INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE E.atomic_number=$1 OR symbol='$1' OR name='$1'")
+if [[ $1 =~ [0-9] ]]
+then
+ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius,symbol FROM elements E INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE E.atomic_number=$1")
+
+else
+ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius,symbol FROM elements E INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+
+fi
 
 if [[ -z $ELEMENT ]]
 then
- echo -e "\nI could not find that element in the database."
+ echo -e I could not find that element in the database.
 else
  
  IFS='|' read ID NAME TYPE ATOMIC_MASS MELTING BOILING SYMBOL <<< $ELEMENT
