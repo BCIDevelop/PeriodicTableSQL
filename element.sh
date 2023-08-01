27cf8273f54e1d9318f27b397e4c817550c3bd02
BCIDevelop <luis.lopez@utec.edu.pe>
2023-08-01 22:43:39 +0000

    test: PSQL search element

--- element.sh
+++ element.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo -e "\nPlease provide an element as an argument.\n"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
+read USER_INPUT
+
+ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements E INNER JOIN properties USING(atomic_nunber) INNER JOIN types USING(type_id) WHERE E.atomic_number=$USER_INPUT OR symbol='$USER_INPUT' OR name='$USER_INPUT'")
+echo $ELEMENT
