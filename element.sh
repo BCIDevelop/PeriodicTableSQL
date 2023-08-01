8c1e933b79eba4dc5e49fd73ed20d8e178891e98
BCIDevelop <luis.lopez@utec.edu.pe>
2023-08-01 23:30:54 +0000

    feat: search periodit table

--- element.sh
+++ element.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-echo -e "\nPlease provide an element as an argument.\n"
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+if [[ $1 ]]
+then
 
-ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements E INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE E.atomic_number=$1 OR symbol='$1' OR name='$1'")
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+ELEMENT=$($PSQL "SELECT E.atomic_number,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius,symbol FROM elements E INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE E.atomic_number=$1 OR symbol='$1' OR name='$1'")
 
 if [[ -z $ELEMENT ]]
 then
  echo -e "\nI could not find that element in the database."
 else
- echo $ELEMENT
+ 
+ IFS='|' read ID NAME TYPE ATOMIC_MASS MELTING BOILING SYMBOL <<< $ELEMENT
+ echo "The element with atomic number 1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+fi
+else 
+  echo Please provide an element as an argument.
 fi
